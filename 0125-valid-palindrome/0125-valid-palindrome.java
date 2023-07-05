2d5a3de9def75955f9d315a46b8cdc71e052f060
Satyam Kumar Navneet <76639713+Satyamkumarnavneet@users.noreply.github.com>
2023-07-05 13:19:31 +0530

    Time: 26 ms (31.23%), Space: 43.6 MB (51.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,27 +1,13 @@
-// class Solution {
-//     public boolean isPalindrome(String s) {
-//         int n = s.length();
-//         s = s.toLowerCase();
-//         for(int i = 0; i<n/2; i++){
-//             if(s.charAt(i) != s.charAt(n-1-i)){
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-// }
-
 class Solution {
     public boolean isPalindrome(String s) {
-        // Remove non-alphanumeric characters and convert to lowercase
         String cleanedString = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         int n = cleanedString.length();
-        
-        for (int i = 0; i < n/2; i++) {
-            if (cleanedString.charAt(i) != cleanedString.charAt(n-1-i)) {
+        for(int i = 0; i<n/2; i++){
+            if(cleanedString.charAt(i) != cleanedString.charAt(n-1-i)){
                 return false;
             }
         }
         return true;
     }
 }
+
