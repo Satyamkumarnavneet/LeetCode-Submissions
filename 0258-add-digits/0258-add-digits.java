1fc7d9ea1689e177009b6a080f4c95fb81e7336c
Satyam Kumar Navneet <76639713+Satyamkumarnavneet@users.noreply.github.com>
2024-03-06 01:48:22 +0530

    Time: 0 ms (100%), Space: 40.7 MB (60.31%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -2,10 +2,7 @@ class Solution {
     public int addDigits(int num) {
         if(num == 0){
             return 0;
-        } else if(num % 9 ==0){
-            return 9;
         }
-        return num % 9;
-        
+        return num % 9 == 0 ? 9 : num % 9;
     }
 }
