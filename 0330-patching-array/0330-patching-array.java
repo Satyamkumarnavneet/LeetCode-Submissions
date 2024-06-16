ce647c7d8bcd900e41f189d30ea29f2b36922ca4
Satyam Kumar Navneet <76639713+Satyamkumarnavneet@users.noreply.github.com>
2024-06-16 23:42:58 +0530

    Time: 0 ms (100%), Space: 44 MB (46.04%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long miss = 1;
+        int count = 0;
+        int i = 0;
+
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) {
+                miss += nums[i++];
+            } else {
+                miss += miss;
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
