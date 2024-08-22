bdc3896966decd6dc5f9731c51960f1f1bc7c9a0
Satyam Kumar Navneet <76639713+Satyamkumarnavneet@users.noreply.github.com>
2024-08-22 19:53:45 +0530

    Time: 6 ms (54.87%), Space: 45.1 MB (13.54%) - LeetHub

---
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.java
@@ -0,0 +1,49 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public TreeNode recoverFromPreorder(String traversal) {
+        Stack<TreeNode> stack = new Stack<>();
+        int idx = 0;
+        
+        while (idx < traversal.length()) {
+            int level = 0;
+            while (traversal.charAt(idx) == '-') {
+                level++;
+                idx++;
+            }
+            
+            int val = 0;
+            while (idx < traversal.length() && Character.isDigit(traversal.charAt(idx))) {
+                val = val * 10 + (traversal.charAt(idx) - '0');
+                idx++;
+            }
+            
+            TreeNode node = new TreeNode(val);
+            while (stack.size() > level) {
+                stack.pop();
+            }
+            
+            if (!stack.isEmpty()) {
+                TreeNode parent = stack.peek();
+                if (parent.left == null) {
+                    parent.left = node;
+                } else {
+                    parent.right = node;
+                }
+            }
+            
+            stack.push(node);
+        }
+        while (stack.size() > 1) {
+            stack.pop();
+        }
+        return stack.pop();
+    }
+}
