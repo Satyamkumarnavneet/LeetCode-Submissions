131a6aaeae341895e6f3156d39e996f26560c46d
Satyam Kumar Navneet <76639713+Satyamkumarnavneet@users.noreply.github.com>
2024-08-23 23:05:45 +0530

    Time: 0 ms (100%), Space: 41.5 MB (79%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] A) {
+        if (A[0][0] == 1) return 0;
+
+        int rows = A.length;
+        int cols = A[0].length;
+        int[][] cache = new int[rows][cols];
+
+        cache[0][0] = 1;
+
+        for (int i = 1; i < rows; i++) {
+            if (A[i][0] == 0)
+                cache[i][0] = cache[i - 1][0];
+        }
+
+        for (int j = 1; j < cols; j++) {
+            if (A[0][j] == 0)
+                cache[0][j] = cache[0][j - 1];
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (A[i][j] == 0)
+                    cache[i][j] = cache[i - 1][j] + cache[i][j - 1];
+            }
+        }
+        return cache[rows - 1][cols - 1];
+    }
+}
