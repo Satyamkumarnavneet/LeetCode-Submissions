4e5feeb0471526204cb31f944dc34232015bf3bc
Satyam Kumar Navneet <76639713+Satyamkumarnavneet@users.noreply.github.com>
2024-11-03 18:54:25 +0530

    Time: 0 ms (100%), Space: 41 MB (94.8%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -3,7 +3,6 @@ class Solution {
         if (s.length() != goal.length()) {
             return false;
         }
-        String temp = s + s;
-        return temp.contains(goal);
+        return (s + s).contains(goal);
     }
 }
