310cc23e16386746e22f1789e31e42049f7c8dcf
Satyam Kumar Navneet <76639713+Satyamkumarnavneet@users.noreply.github.com>
2025-10-03 21:50:49 +0530

    Time: 20 ms (51.73%), Space: 46.3 MB (71.37%) - LeetHub

---
+++ LeetCode/Hard/0407-trapping-rain-water-ii/0407-trapping-rain-water-ii-10-03-2025-21-50-48.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int trapRainWater(int[][] heightMap) {
+        if (heightMap == null || heightMap.length == 0 || heightMap[0].length == 0) {
+            return 0;
+        }
+        int m = heightMap.length;
+        int n = heightMap[0].length;
+        boolean[][] visited = new boolean[m][n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    pq.offer(new int[]{i, j, heightMap[i][j]});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        int water = 0;
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        while (!pq.isEmpty()) {
+            int[] cell = pq.poll();
+            for (int[] dir : dirs) {
+                int x = cell[0] + dir[0];
+                int y = cell[1] + dir[1];
+                if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]) {
+                    visited[x][y] = true;
+                    water += Math.max(0, cell[2] - heightMap[x][y]);
+                    pq.offer(new int[]{x, y, Math.max(heightMap[x][y], cell[2])});
+                }
+            }
+        }
+        return water;
+    }
+}
