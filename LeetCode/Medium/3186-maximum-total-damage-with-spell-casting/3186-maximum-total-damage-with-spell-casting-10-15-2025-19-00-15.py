83b147c454611dc04c2356a1382500a0bd3568b3
Satyam Kumar Navneet <76639713+Satyamkumarnavneet@users.noreply.github.com>
2025-10-15 19:00:25 +0530

    Time: 461 ms (74.75%), Space: 47.7 MB (21.43%) - LeetHub

---
+++ LeetCode/Medium/3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting-10-15-2025-19-00-15.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maximumTotalDamage(self, power: List[int]) -> int:
+        count = Counter(power)
+        unique = sorted(count.keys())
+        total = {x: x * count[x] for x in unique}
+
+        n = len(unique)
+        dp = [0] * n
+
+        for i in range(n):
+            take = total[unique[i]]
+            j = i - 1
+            while j >= 0 and unique[j] >= unique[i] - 2:
+                j -= 1
+            if j >= 0:
+                take += dp[j]
+            skip = dp[i-1] if i > 0 else 0
+            dp[i] = max(skip, take)
+
+        return dp[-1]
